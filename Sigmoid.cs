0f4e52cb3152982bf259d912d9b9cf44395311e5
Masaaki Kawata <kawatan@gmail.com>
2016-11-30 03:35:22 +0900

    Add activation functions

---
+++ Sigmoid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Milk
+{
+    namespace ActivationFunctions
+    {
+        public class Sigmoid : IActivationFunction
+        {
+            public double Activate(double x)
+            {
+                return 1.0 / (1.0 + Math.Exp(-x));
+            }
+
+            public double Derivative(double x)
+            {
+                return x * (1.0 - x);
+            }
+        }
+    }
+}
