92a5bbc7eb466503fd176a7ddfac4e215329a756
Masaaki Kawata <kawatan@gmail.com>
2018-03-12 01:50:12 +0900

    Add the cross-entropy loss function for binaly classification

--- Alice/LossFunctions/BinaryCrossEntropy.cs
+++ Alice/LossFunctions/BinaryCrossEntropy.cs
@@ -4,17 +4,17 @@ namespace Alice
 {
     namespace LossFunctions
     {
-        // Mean squared error (MSE) for regression
-        public class MeanSquaredError : ILossFunction
+        // Cross-entropy loss function for binary classification
+        public class BinaryCrossEntropy : ILossFunction
         {
             public double Function(double y, double a)
             {
-                return (y - a) * (y - a) / 2;
+                return -a * Math.Log(y, Math.E) - (1.0 - a) * Math.Log(1.0 - a, Math.E);
             }
 
             public double Derivative(double y, double a)
             {
-                return y - a;
+                return (y - a) / (y * (1.0 - y));
             }
         }
     }
--- Alice/LossFunctions/CategoricalCrossEntropy.cs
+++ Alice/LossFunctions/CategoricalCrossEntropy.cs
@@ -4,17 +4,17 @@ namespace Alice
 {
     namespace LossFunctions
     {
-        // Mean squared error (MSE) for regression
-        public class MeanSquaredError : ILossFunction
+        // Cross-entropy loss function for multiclass classification
+        public class CategoricalCrossEntropy : ILossFunction
         {
             public double Function(double y, double a)
             {
-                return (y - a) * (y - a) / 2;
+                return -a * Math.Log(y, Math.E);
             }
 
             public double Derivative(double y, double a)
             {
-                return y - a;
+                return -a / y;
             }
         }
     }
