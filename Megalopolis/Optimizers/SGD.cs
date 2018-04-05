405069dd9830c115bc4fe043a180b67e8370c066
Masaaki Kawata <kawatan@gmail.com>
2018-04-05 13:05:28 +0900

    Add SGD

---
+++ Megalopolis/Optimizers/SGD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Megalopolis.Optimizers
+{
+    public class SGD
+    {
+        public double lr = 0.001; // Learning rate
+
+        public SGD() { }
+
+        public SGD(double lr)
+        {
+            this.lr = lr;
+        }
+
+        public double Optimize(int index, double weight, double gradient)
+        {
+            return weight - this.lr * gradient;
+        }
+    }
+}
